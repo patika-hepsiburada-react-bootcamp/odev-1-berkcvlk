f05051076ecc5eb04f846d3296a33abb8708a8aa
Berk Civelek <berkcvlk@gmail.com>
2021-09-20 16:04:34 +0300

    Created function that get user and posts

---
+++ src/lib/service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const fetcher = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+/**
+ * Kullanıcı id'ye göre ilgili postları getirecek fonksiyon
+ * @param {number} id Kullanıcı id
+ * @returns {Object} Kullanıcı ve kullanıcıya ait post bilgileri
+ */
+const getData = async (id) => {
+  try {
+    const { data: user } = await fetcher(`/users/${id}`);
+    const { data: posts } = await fetcher(`/posts?${user.id}`);
+
+    return { ...user, posts: posts };
+  } catch (err) {
+    return err.message;
+  }
+};
+
+export default getData;
